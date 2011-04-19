d8d08f0f0f8bd04e2dc258d4137246ebeca73658
rokon.richardo@gmail.com <rokon.richardo@gmail.com@92683023-e08e-1e24-f86a-e7231671aef1>
2011-04-19 14:51:14 +0000

    + added a j2me version src * modified LevelX render and constructor

--- trunk/src/com/mind/goldminer/screen/LevelX.java
+++ trunk/src/com/mind/goldminer/screen/LevelX.java
@@ -1,21 +1,60 @@
 package com.mind.goldminer.screen;
 
+import javax.microedition.khronos.opengles.GL10;
+
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Matrix4;
 
 public class LevelX implements Screen{
 
 	public ScreenState nextState;
 	
+	private final SpriteBatch 	spriteBatch;
+	private final Texture 		background;
+	private final Texture		miner;
+	private final Texture		gold;
+	
+	private final Matrix4		viewMatrix;
+	private final Matrix4		transformMatrix;
+	
 	public LevelX() {
-		
+		Gdx.app.log("LevelX", "LevelX()");
 		nextState = ScreenState.Shop;
-		Gdx.app.log("LevelX", "LevelX()");	
+		
+		spriteBatch = new SpriteBatch();
+		background = new Texture(Gdx.files.internal("data/background.png"));
+		background.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		miner = new Texture(Gdx.files.internal("data/miner.png"));
+		miner.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		gold = new Texture(Gdx.files.internal("data/gold.png"));
+		gold.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		
+		viewMatrix = new Matrix4();
+		transformMatrix = new Matrix4();
 	}
 	
 	@Override
 	public void render() {
 		// TODO Auto-generated method stub
 		Gdx.app.log("LevelX", "render()");
+		Application app = Gdx.app;
+		
+		app.getGraphics().getGL10().glClear(GL10.GL_COLOR_BUFFER_BIT);
+		
+		viewMatrix.setToOrtho2D(0, 0, 800, 480);
+		spriteBatch.setProjectionMatrix(viewMatrix);
+		spriteBatch.setTransformMatrix(transformMatrix);
+		spriteBatch.begin();
+		spriteBatch.disableBlending();
+		spriteBatch.draw(background, 0, 0, 800, 480, 0, 0, 512, 512, false, false);
+		spriteBatch.enableBlending();
+		spriteBatch.draw(miner, 400, 400);
+		spriteBatch.draw(gold, 300, 200);
+		spriteBatch.end();
 	}
 
 	@Override
