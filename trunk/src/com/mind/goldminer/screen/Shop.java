9afbd85e67d2c7f81e83c1ee90daea65423f46e3
rokon.richardo@gmail.com <rokon.richardo@gmail.com@92683023-e08e-1e24-f86a-e7231671aef1>
2011-04-20 08:35:39 +0000

    * revert to version 14 and ignore bin directory

---
+++ trunk/src/com/mind/goldminer/screen/Shop.java
@@ -0,0 +1,54 @@
+package com.mind.goldminer.screen;
+
+import com.badlogic.gdx.Gdx;
+
+public class Shop implements Screen{
+
+	public ScreenState nextState;
+	
+	public Shop() {
+		
+		nextState = ScreenState.GameOver;
+		Gdx.app.log("Shop", "Shop()");
+	}
+	
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+		Gdx.app.log("Shop", "render()");
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		Gdx.app.log("Shop", "update()");
+	}
+
+	@Override
+	public void setNextState(ScreenState screenState) {
+		// TODO Auto-generated method stub
+		Gdx.app.log("Shop", "setNextState()");
+		nextState = screenState;
+	}
+
+	@Override
+	public ScreenState nextState() {
+		// TODO Auto-generated method stub
+		Gdx.app.log("Shop", "nextState()");
+		return nextState;
+	}
+
+	@Override
+	public boolean isDone() {
+		// TODO Auto-generated method stub
+		Gdx.app.log("Shop", "isDone()");
+		return Gdx.input.isTouched();
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
