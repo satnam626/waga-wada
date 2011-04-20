71e24414fd2c8be0f3301092994ded51e4d6d23d
rokon.richardo <rokon.richardo@gmail.com>
2011-04-20 08:55:12 +0000

    - commented render codes in LevelX.java + added state switch code in LevelX.java

--- src/com/mind/goldminer/screen/LevelX.java
+++ src/com/mind/goldminer/screen/LevelX.java
@@ -13,48 +13,60 @@ public class LevelX implements Screen{
 
 	public ScreenState nextState;
 	
-	private final SpriteBatch 	spriteBatch;
-	private final Texture 		background;
-	private final Texture		miner;
-	private final Texture		gold;
+	private enum LevelState { Level_Loading,
+							Level_Play, 
+							Level_Failed,
+							Level_Complete, 
+							Level_Shop,
+							};
 	
-	private final Matrix4		viewMatrix;
-	private final Matrix4		transformMatrix;
+	private LevelState			curLevelState;
+	private LevelState			nextLevelState;
+							
+//	private final SpriteBatch 	spriteBatch;
+//	private final Texture 		background;
+//	private final Texture		miner;
+//	private final Texture		gold;
+//	
+//	private final Matrix4		viewMatrix;
+//	private final Matrix4		transformMatrix;
 	
 	public LevelX() {
 		Gdx.app.log("LevelX", "LevelX()");
-		nextState = ScreenState.Shop;
+//		nextState = ScreenState.Shop;
+		curLevelState = LevelState.Level_Loading;
+		nextLevelState = LevelState.Level_Play;
 		
-		spriteBatch = new SpriteBatch();
-		background = new Texture(Gdx.files.internal("data/background.png"));
-		background.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		miner = new Texture(Gdx.files.internal("data/miner.png"));
-		miner.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		gold = new Texture(Gdx.files.internal("data/gold.png"));
-		gold.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+//		spriteBatch = new SpriteBatch();
+//		background = new Texture(Gdx.files.internal("data/background.png"));
+//		background.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+//		miner = new Texture(Gdx.files.internal("data/miner.png"));
+//		miner.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+//		gold = new Texture(Gdx.files.internal("data/gold.png"));
+//		gold.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 		
-		viewMatrix = new Matrix4();
-		transformMatrix = new Matrix4();
+//		viewMatrix = new Matrix4();
+//		transformMatrix = new Matrix4();
 	}
 	
 	@Override
 	public void render() {
 		// TODO Auto-generated method stub
 		Gdx.app.log("LevelX", "render()");
-		Application app = Gdx.app;
-		
-		app.getGraphics().getGL10().glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
-		viewMatrix.setToOrtho2D(0, 0, 800, 480);
-		spriteBatch.setProjectionMatrix(viewMatrix);
-		spriteBatch.setTransformMatrix(transformMatrix);
-		spriteBatch.begin();
-		spriteBatch.disableBlending();
-		spriteBatch.draw(background, 0, 0, 800, 480, 0, 0, 512, 512, false, false);
-		spriteBatch.enableBlending();
-		spriteBatch.draw(miner, 400, 400);
-		spriteBatch.draw(gold, 300, 200);
-		spriteBatch.end();
+//		Application app = Gdx.app;
+//		
+//		app.getGraphics().getGL10().glClear(GL10.GL_COLOR_BUFFER_BIT);
+//		
+//		viewMatrix.setToOrtho2D(0, 0, 800, 480);
+//		spriteBatch.setProjectionMatrix(viewMatrix);
+//		spriteBatch.setTransformMatrix(transformMatrix);
+//		spriteBatch.begin();
+//		spriteBatch.disableBlending();
+//		spriteBatch.draw(background, 0, 0, 800, 480, 0, 0, 512, 512, false, false);
+//		spriteBatch.enableBlending();
+//		spriteBatch.draw(miner, 400, 400);
+//		spriteBatch.draw(gold, 300, 200);
+//		spriteBatch.end();
 	}
 
 	@Override
